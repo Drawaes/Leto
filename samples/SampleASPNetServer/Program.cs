0d1b9e4d20c6469450f13183af9e97e5580a76a4
Drawaes <seawardtim@gmail.com>
2017-02-11 03:24:07 +0000

    VS 2017 Upgrade

--- samples/SampleASPNetServer/Program.cs
+++ samples/SampleASPNetServer/Program.cs
@@ -14,11 +14,22 @@ namespace SampleASPNetServer
     {
         public static void Main(string[] args)
         {
+            if (args == null || args.Length < 1 || (args[0] != "leto" && args[0] != "sslstream"))
+            {
+                Console.WriteLine("leto or sslstream is required");
+                return;
+            }
             var host = new WebHostBuilder()
                 .UseKestrel((ops) =>
                 {
-                    //ops.UseHttps(".\\data\\new.pfx", "Test123t");
-                    ops.UseLetoHttps(".\\data\\new.pfx", "Test123t");
+                    if (args[0] == "leto")
+                    {
+                        ops.UseLetoHttps(".\\data\\new.pfx", "Test123t");
+                    }
+                    else
+                    {
+                        ops.UseHttps(".\\data\\new.pfx", "Test123t");
+                    }
                 })
                 .UseUrls("https://localhost:443")
                 .UseContentRoot(Directory.GetCurrentDirectory())
