34b77a99b3bd410c7f8fcdd8817be5b55e888d90
Drawaes <seawardtim@gmail.com>
2017-03-12 19:18:56 +0000

    Done ec

--- src/Leto.OpenSsl11/OpenSslKeyshareProvider.cs
+++ src/Leto.OpenSsl11/OpenSslKeyshareProvider.cs
@@ -11,17 +11,17 @@ namespace Leto.OpenSsl11
         {
             switch (namedGroup)
             {
+                case NamedGroup.secp256r1:
+                case NamedGroup.secp384r1:
+                case NamedGroup.secp521r1:
+                    return new OpenSslECCurveKeyshare(namedGroup);
                 case NamedGroup.ffdhe2048:
                 case NamedGroup.ffdhe3072:
                 case NamedGroup.ffdhe4096:
                 case NamedGroup.ffdhe6144:
                 case NamedGroup.ffdhe8192:
-                    return new FiniteFieldKeyshare(namedGroup);
-                case NamedGroup.secp256r1:
-                    return new OpenSslECCurveKeyshare(namedGroup);
                 case NamedGroup.x25519:
                 case NamedGroup.x448:
-                    return new ECFunctionKeyshare(namedGroup);
                 default:
                     return null;
             }
