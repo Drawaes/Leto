aae86f11f25473aae3f3bd5c792882295b973164
Drawaes <seawardtim@gmail.com>
2017-03-25 15:32:08 +0000

    broken

--- src/Leto/Handshake/Extensions/ApplicationLayerProtocolProvider.cs
+++ src/Leto/Handshake/Extensions/ApplicationLayerProtocolProvider.cs
@@ -28,7 +28,7 @@ namespace Leto.Handshake.Extensions
         private (ApplicationLayerProtocolType, byte[])[] _supportedProtocols;
         private bool _serverListTakesPriority = true;
 
-        public ApplicationLayerProtocolProvider(params ApplicationLayerProtocolType[] supportedProtocols)
+        public ApplicationLayerProtocolProvider(ApplicationLayerProtocolType[] supportedProtocols)
         {
             if (supportedProtocols?.Length > 0)
             {
