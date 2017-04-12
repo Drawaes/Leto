a0ebed41cc50a7833984db6844bf8583ff9ceff1
Drawaes <seawardtim@gmail.com>
2017-04-13 00:50:50 +0100

    tests

---
+++ script/runtests.bat
@@ -0,0 +1,3 @@
+dotnet test -c Debug test/Leto.ProtocolFacts/Leto.ProtocolFacts.csproj
+dotnet test -c Debug test/Leto.WindowsFacts/Leto.WindowsFacts.csproj
+dotnet test -c Debug test/Leto.OpenSslFacts/Leto.OpenSslFacts.csproj
