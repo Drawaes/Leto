8923a02e4d96d8768996dd78c8f43de7a57f35d9
Drawaes <seawardtim@gmail.com>
2017-04-14 22:37:15 +0100

    Update test harness

--- script/runtests.bat
+++ script/runtests.bat
@@ -1,2 +1,3 @@
 dotnet test -c Debug test/Leto.ProtocolFacts/Leto.ProtocolFacts.csproj
 dotnet test -c Debug test/Leto.OpenSslFacts/Leto.OpenSslFacts.csproj
+dotnet test -c Debug test/Leto.WindowsFacts/Leto.WindowsFacts.csproj
