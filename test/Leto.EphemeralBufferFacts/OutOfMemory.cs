e3d212c67f871b86bb7d3c2f6f561a93897f3118
drawaes <seawardtim@gmail.com>
2017-04-22 23:50:50 +0100

    More Tests

--- test/Leto.EphemeralBufferFacts/OutOfMemory.cs
+++ test/Leto.EphemeralBufferFacts/OutOfMemory.cs
@@ -13,5 +13,23 @@ namespace Leto.EphemeralBufferFacts
                 var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(10000, 10000, false);
             });
         }
+
+        [Fact]
+        public void InvalidNumberOfBuffersRequested()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(100, -1);
+            });
+        }
+
+        [Fact]
+        public void InvalidBufferSizeRequested()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(0, 100);
+            });
+        }
     }
 }
