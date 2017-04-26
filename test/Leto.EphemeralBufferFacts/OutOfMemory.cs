9fbb017111f4c132eb10b6bf75d33f85c8b42715
Drawaes <seawardtim@gmail.com>
2017-04-26 19:42:47 +0100

    Formatting

--- test/Leto.EphemeralBufferFacts/OutOfMemory.cs
+++ test/Leto.EphemeralBufferFacts/OutOfMemory.cs
@@ -6,31 +6,22 @@ namespace Leto.EphemeralBufferFacts
     public class OutOfMemory
     {
         [Fact]
-        public void FailDueToWorkingSetSize()
-        {
-            Assert.Throws<InvalidOperationException>(() =>
+        public void FailDueToWorkingSetSize() => Assert.Throws<InvalidOperationException>(() =>
             {
                 var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(10000, 10000, false);
             });
-        }
 
         [Fact]
-        public void InvalidNumberOfBuffersRequested()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
+        public void InvalidNumberOfBuffersRequested() => Assert.Throws<ArgumentOutOfRangeException>(() =>
             {
                 var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(100, -1);
             });
-        }
 
         [Fact]
-        public void InvalidBufferSizeRequested()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
+        public void InvalidBufferSizeRequested() => Assert.Throws<ArgumentOutOfRangeException>(() =>
             {
                 var pool = EphemeralBuffers.EphemeralBufferPool.CreateBufferPool(0, 100);
             });
-        }
 
         [Fact]
         public void RequestedBufferIsTooLarge()
