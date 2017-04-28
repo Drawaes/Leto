299247bfbcc63cbcc29bbecf4e4cc6718dff62aa
Drawaes <seawardtim@gmail.com>
2017-04-28 19:12:50 +0100

    Added test to check handling of hello message with "extra" bytes at the end

---
+++ test/Leto.OpenSslFacts/BadMessageFacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Leto.OpenSslFacts
+{
+    public class BadMessageFacts
+    {
+        [Fact]
+        public async Task ClientHelloWithExtraBytes()
+        {
+            using (var listener = new OpenSsl11.OpenSslSecurePipeListener(Data.Certificates.RSACertificate))
+            {
+                await CommonFacts.BadHelloFacts.SendHelloWithExtraTrailingBytes(listener);
+            }
+        }
+    }
+}
