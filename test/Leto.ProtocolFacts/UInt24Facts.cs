5c6c3bd3f99d41812ee5720a7966fd899c33247b
Drawaes <seawardtim@gmail.com>
2017-04-28 03:59:44 +0100

    Uint24 facts

---
+++ test/Leto.ProtocolFacts/UInt24Facts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leto.ProtocolFacts
+{
+    public class UInt24Facts
+    {
+        [Theory]
+        [InlineData(ushort.MaxValue)]
+        [InlineData(ushort.MinValue)]
+        [InlineData((ushort)1)]
+        public void UshortConvertsForwardAndBack(ushort valueToCheck)
+        {
+            Internal.UInt24 newValue = valueToCheck;
+            var returnValue = (ushort)newValue;
+            Assert.Equal(valueToCheck, returnValue);
+        }
+
+        [Theory]
+        [InlineData(ushort.MaxValue)]
+        [InlineData(ushort.MinValue)]
+        [InlineData((ushort)1)]
+        public void UshortConvertsForwardAndBackToInt(int valueToCheck)
+        {
+            Internal.UInt24 newValue = (ushort)valueToCheck;
+            int returnValue = newValue;
+            Assert.Equal(valueToCheck, returnValue);
+        }
+    }
+}
