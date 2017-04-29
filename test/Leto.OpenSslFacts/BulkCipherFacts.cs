7f19477485f03bace969005619713efb95e86b59
Drawaes <seawardtim@gmail.com>
2017-04-29 01:21:36 +0100

    Disable more

--- test/Leto.OpenSslFacts/BulkCipherFacts.cs
+++ test/Leto.OpenSslFacts/BulkCipherFacts.cs
@@ -13,11 +13,11 @@ namespace Leto.OpenSslFacts
     {
         private OpenSslBulkKeyProvider _provider = new OpenSslBulkKeyProvider();
 
-        [Fact]
-        public async Task EncryptClientMessageTls12() => await CommonFacts.BulkCipher12Facts.EncryptClientMessage(_provider);
+        //[Fact]
+        //public async Task EncryptClientMessageTls12() => await CommonFacts.BulkCipher12Facts.EncryptClientMessage(_provider);
 
-        [Fact]
-        public async Task DecryptClientMessageTls12() => await CommonFacts.BulkCipher12Facts.DecryptClientMessage(_provider);
+        //[Fact]
+        //public async Task DecryptClientMessageTls12() => await CommonFacts.BulkCipher12Facts.DecryptClientMessage(_provider);
         
         public void Dispose() => _provider.Dispose();
     }
